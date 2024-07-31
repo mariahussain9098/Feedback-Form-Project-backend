354b08237e74d9ba9e383f6dd58df395bf2a12fd
maria <mariahussain9098@gmail.com>
2024-07-31 16:58:46 +0500

    changes in index file

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use('/api/teachers', teacherRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/feedbackForm', feedbackFormRoutes);
 
+// port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
