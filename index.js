1accd6659e54eca6fcc748a7a0b0e27fdd8d1f72
maria <mariahussain9098@gmail.com>
2024-08-02 14:39:41 +0500

    auth controller modified

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-// const express = require('express');
-// require('dotenv').config();
-// const dbConnect = require('./src/config/dbConnect');
-// const authRoutes = require('./src/routes/authRoutes');
-// const feedbackRoutes = require('./src/routes/feedbackRoutes');
-// const studentRoutes = require('./src/routes/studentRoutes');
-// const teacherRoutes = require('./src/routes/teacherRoutes');
-// const adminRoutes = require('./src/routes/adminRoutes');
-// const feedbackFormRoutes = require('./src/routes/feedbackFormRoutes');
-
-// const app = express();
-
-// app.use(express.json());
-// dbConnect();
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/feedback', feedbackRoutes);
-// app.use('/api/students', studentRoutes);
-// app.use('/api/teachers', teacherRoutes);
-// app.use('/api/admin', adminRoutes);
-// app.use('/api/feedbackForm', feedbackFormRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-
-
-
-
-
-
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -48,13 +12,13 @@ const feedbackFormRoutes = require('./src/routes/feedbackFormRoutes');
 const app = express();
 
 app.use(express.json());
-// const corsConfig = {
-//   origin : "*",
-//   credential: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// }
-// app.options("", cors(corsConfig))
-app.use(cors());
+const corsConfig = {
+  origin : "*",
+  credential: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}
+app.options("", cors(corsConfig))
+// app.use(cors());
 
 // Database connection
 dbConnect()
