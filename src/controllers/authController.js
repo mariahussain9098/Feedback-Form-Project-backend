101cb867d4f1fd43afc3b4ef3c44e0c0fa6f3400
maria <mariahussain9098@gmail.com>
2024-08-02 11:18:19 +0500

    api endpoints modified

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,26 @@ const jwt = require('jsonwebtoken');
 const Student = require('../models/Student');
 
 
+// const loginStudent = async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     const student = await Student.findOne({ email });
+//     if (!student) {
+//       return res.status(400).json({ message: 'Invalid credentials' });
+//     }
+//     const isMatch = await bcrypt.compare(password, student.password);
+//     if (!isMatch) {
+//       return res.status(400).json({ message: 'Invalid credentials' });
+//     }
+//     const token = jwt.sign({ id: student._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+//     res.json({ token });
+//   } catch (error) {
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// };
+
+
+
 const loginStudent = async (req, res) => {
   const { email, password } = req.body;
   try {
