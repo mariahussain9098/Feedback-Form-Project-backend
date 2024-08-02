f7405e2c05bfadc4d16be240128a026be635f5ca
maria <mariahussain9098@gmail.com>
2024-08-02 14:59:51 +0500

    cors added.....

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const corsConfig = {
   credential: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }
-// app.options("", cors(corsConfig))
+app.options("", cors(corsConfig))
 app.use(cors(corsConfig));
 
 // Database connection
